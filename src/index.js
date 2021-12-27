85f69be8f46a0d31b7633f7a3a77ca4cee204632
Junior Emmnauel Baez Tavarez <82235652+JuniorBaez-ux@users.noreply.github.com>
2021-12-27 13:23:34 -0400

    Pagina a realizar con el webpack.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import './style.css';
+
+console.log("Tamo funcionando klk");
+
+function component() {
+    //const element = document.createElement('div');
+ 
+    // Lodash, now imported by this script
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    
+ 
+    return element;
+  }
+ 
+  document.body.appendChild(component());
