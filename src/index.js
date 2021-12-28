86144edb2bb458a5d3cb4301b078a7c805e6a4eb
Junior Emmnauel Baez Tavarez <82235652+JuniorBaez-ux@users.noreply.github.com>
2021-12-28 10:43:14 -0400

    Creating a specific module for the content of the home display.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import _, { fill } from 'lodash';
 import './style.css';
+import { createHomePage, createTitle } from "./home.js";
+import { createNavBar } from "./home.js";
+import { createFillerText } from "./home.js";
+import { createText } from "./home.js";
 
 let principal = document.getElementById("content");
 
@@ -17,43 +21,4 @@ console.log("Tamo funcionando klk");
  
 //   document.body.appendChild(component());
 
-document.body.appendChild(createTitle());
-document.body.appendChild(createNavBar());
-principal.appendChild(createFillerText());
-principal.appendChild(createFillerText());
-
-  function createTitle(){
-    let title = document.createElement('div');
-    title.className = "title";
-    title.id = "title";
-    title.innerHTML = "The Dominican Palace";
-
-    return title;
-  }
-
-  function createNavBar(){
-    let navbar = document.createElement("div");
-    navbar.className = "topnav";
-    navbar.innerHTML = "<a href='#'>Home</a>";
-    navbar.innerHTML += "<a href='#'>Menu</a>";
-    navbar.innerHTML += "<a href='#'>Contact</a>";
-
-    return navbar;
-  }
-
-  function createFillerText(){
-    let fillerText = document.createElement("div");
-    fillerText.className = "fillertext";
-    fillerText.id = "fillertext";
-    fillerText.appendChild(createText());
-
-    return fillerText;
-}
-
-function createText(){
-    let text = document.createElement("p");
-    text.innerHTML = "The Dominican Palace is a restaurant located in the heart of the city of San Juan,it is a place where you can enjoy a variety of dishes from the best chefs in the city,the restaurant is open from 11:00 AM to 11:00 PM.";
-    
-    return text;
-}
-
+createHomePage();
